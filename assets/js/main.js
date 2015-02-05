2072ffff105b1b6ed2d9c8a9ef07c0f58ad31210
BestThemeRoad <BestThemeRoad@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-05 02:58:41 +0000

    Please rated the plugin if you like it.
    git-svn-id: https://plugins.svn.wordpress.org/perfect-client-testimonial/trunk@1082793 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function() {
+ 
+ jQuery("#owl-demo").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+ 
+});
